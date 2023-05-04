252e257a2fefb59d062ffc0f7f431863d72ec9a5
sny <127958265+kyofu95@users.noreply.github.com>
2023-05-04 18:04:59 +0300

    add a token scheme

---
+++ shortly/schemas/token.py
@@ -0,0 +1,20 @@
+"""This module defines a Pydantic schema for a Token object."""
+
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class TokenPayload(BaseModel):
+    """A Pydantic model representing the payload of a token."""
+    token_type: str
+    exp: datetime
+    iat: Optional[datetime]
+    sub: int
+
+
+class Token(BaseModel):
+    """A Pydantic model representing a security token."""
+    access_token: str
+    token_type: str
