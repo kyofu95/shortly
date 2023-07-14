04bc21c76eb1118ae3d3e2780005d62cc00eadf9
sny <127958265+kyofu95@users.noreply.github.com>
2023-07-14 23:38:41 +0300

    generate key based on id

--- shortly/service/link.py
+++ shortly/service/link.py
@@ -1,35 +1,42 @@
 """This module contains link service."""
 
 import string
-import random
 
 from shortly.schemas.link import LinkInDB
 from shortly.repository.link import LinkRepository, GenerationFailed
 
-ALPHANUMERIC: str = string.ascii_letters + string.digits
-MAX_NUM_OF_TRIES = 5
+ALPHANUMERIC: str = string.digits + string.ascii_letters
+ALPHANUMERIC_LEN: int = len(ALPHANUMERIC)
 
 
 class CreateLinkError(Exception):
     """Raised when lnik could not be created."""
 
 
+def encode_base62(number: int) -> str:
+    """Encodes a given number into a base62 string."""
+
+    if not number:
+        return "0"
+
+    encoded = ""
+    while number:
+        number, rem = divmod(number, ALPHANUMERIC_LEN)
+        encoded = ALPHANUMERIC[rem] + encoded
+    return encoded
+
+
 async def create(original_url: str, user_id: int, repo: LinkRepository) -> LinkInDB:
-    """"
+    """ "
     Creates link.
-    Tries to iterate link generation when unique check fails.
     """
 
-    while True:
-        retry_number = MAX_NUM_OF_TRIES
-        try:
-            key = "".join(random.choices(ALPHANUMERIC, k=7))
-            link = await repo.create(key, original_url, user_id)
-        except GenerationFailed as exc:
-            retry_number -= 1
-            if not retry_number:
-                raise CreateLinkError() from exc
-        else:
-            break
+    link_id = await repo.get_id_from_sequence()
+
+    key = encode_base62(link_id)
+    try:
+        link = await repo.create(link_id, key, original_url, user_id)
+    except GenerationFailed as exc:
+        raise CreateLinkError() from exc
 
     return link
